ab5dbc7dc1c6b64a420916e81d210e0616ebfdb0
Doug <doug.steinberg@gmail.com>
2017-07-11 15:50:14 -0400

    Exercice complete

--- election.js
+++ election.js
@@ -1,5 +1,24 @@
+// In the election.js file, build an AJAX request that makes a GET request to the root path, and expects a JSON response.
+// Make sure you're making this request within your document.addEventListener("DOMContentLoaded"...) callback function
+// Add a <ul> to your index.html file, with an id. This is what we'll attach our data to.
+
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  var summonCandidates = document.querySelectorAll('responseData.candidates')
 
+  runSummonCandidates = document.addEventListener('click', function(event) {
+  console.log('click')
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(data) {
+    console.log(data)
+    for(var i = 0; i < data.candidates.length; i++) {
+      var element = document.createElement('li');
+      element.innerHTML = 'Name: ' + data.candidates[i].name + "<br>Votes:  " + data.candidates[i].votes + "<br><br>";
+      document.querySelector('#ids').append(element);
+      }
+    });
+  });
 });
